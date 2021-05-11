29ef70a9b3c6ad54669908032f16dd6110cd939c
WaruGameDev <walter.veneros@ulpomedia.com>
2021-05-11 13:17:31 -0400

    clase 11 mayo

---
+++ Assets/ChangeOnMist.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeOnMist : MonoBehaviour
+{
+    public Sprite original, mistVersion;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Mist"))
+        {
+            GetComponent<SpriteRenderer>().sprite = mistVersion;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Mist"))
+        {
+            GetComponent<SpriteRenderer>().sprite = original;
+        }
+    }
+}
---
+++ Assets/SlowPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowPlayer : MonoBehaviour
+{
+    private float originalSpeed;
+    private float originalGravity;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            originalSpeed = other.GetComponent<Movement>().speed;
+            originalGravity = other.GetComponent<Rigidbody2D>().gravityScale;
+            other.GetComponent<Rigidbody2D>().gravityScale /= 2;
+            other.GetComponent<Movement>().speed /= 2;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            
+            other.GetComponent<Movement>().speed = originalSpeed;
+            other.GetComponent<Rigidbody2D>().gravityScale = originalGravity;
+        }
+    }
+}
